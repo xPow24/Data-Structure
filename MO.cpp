7eecd39b0a432bc7f75ddcfbe7668c97ff600d45
Lê Trọng Bảo <114853668+xPow24@users.noreply.github.com>
2023-11-01 22:18:50 +0700

    Update MO.cpp

--- MO.cpp
+++ MO.cpp
@@ -19,27 +19,27 @@ const int logn = 20;
 const int maxn = 1 << logn;
  
 int gilbertOrder(int x, int y) {
-    const int logn = __lg(max(x, y) * 2 + 1) | 1;
-    const int maxn = (1ll << logn) - 1;
-    int res = 0;
-    for (int s = 1ll << (logn - 1); s; s >>= 1) {
-        bool rx = x & s, ry = y & s;
-        res = (res << 2) | (rx ? ry ? 2 : 1 : ry ? 3 : 0);
-        if (!rx) {
-            if (ry) x ^= maxn, y ^= maxn;
-            swap(x, y);
-        }
+  const int logn = __lg(max(x, y) * 2 + 1) | 1;
+  const int maxn = (1ll << logn) - 1;
+  int res = 0;
+  for (int s = 1ll << (logn - 1); s; s >>= 1) {
+    bool rx = x & s, ry = y & s;
+    res = (res << 2) | (rx ? ry ? 2 : 1 : ry ? 3 : 0);
+    if (!rx) {
+      if (ry) x ^= maxn, y ^= maxn;
+      swap(x, y);
     }
-    return res;
+  }
+  return res;
 }
  
 struct Query {
-	int l, r, idx;
-	int ord;
+  int l, r, idx;
+  int ord;
 	
-	void calcOrder() {
-		ord = gilbertOrder(l, r);
-	}
+  void calcOrder() {
+    ord = gilbertOrder(l, r);
+  }
 
   bool operator < (const Query& A) const {
     return ord < A.ord;
